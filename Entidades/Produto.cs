3ba7882cb0293131ef60972a1b54541edf763a17
Joao_amd <joao07medeiros@gmail.com>
2022-12-01 21:56:40 -0300

    Adicionar primeria versao

---
+++ Entidades/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedido.Entidades
+{
+    internal class Produto
+    {
+    }
+}
